b648568d60c939f860ddbcd557f8a4d00ddba880
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-08-10 02:34:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,7 @@ def monopoly_with_third_tier
        :reading_railroad => {},
        :pennsylvania_railroad => {},
        :b_and_o_railroad => {},
-       :shortline => {},
+       :shortline_railroad => {},
 	  }
    }
   }
@@ -44,7 +44,7 @@ def monopoly_with_fourth_tier
        :reading_railroad => {"mortgage_value" => "$100"},
        :pennsylvania_railroad => {"mortgage_value" => "$200"},
        :b_and_o_railroad => {"mortgage_value" => "$400"},
-       :shortline => {"mortgage_value" => "$800"},
+       :shortline_railroad => {"mortgage_value" => "$800"},
 	  }
    }
   }
